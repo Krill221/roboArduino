667fcd159d83046df7c2b1ca1a29ab960b4ccc1a
kirilleropkin <krill221@gmail.com>
2019-05-28 11:03:56 +0300

    robo wheels

--- RoboWheels/RoboWheels.ino
+++ RoboWheels/RoboWheels.ino
@@ -19,8 +19,8 @@ void setup() {
   Serial.begin(9600);
 
   // wheels init
-  myservo1.attach(12); myservo1.write(90);
-  myservo2.attach(13); myservo2.write(90);
+  myservo1.attach(5); myservo1.write(90);
+  myservo2.attach(6); myservo2.write(90);
 
   // sensors init
   Wire.begin();
@@ -64,18 +64,20 @@ void loop() {
 //////
 
 void shift_to_center() { // add error to 10
+    Serial.println("hard shift_to_center");
     // 1. hard shift to center circle (double lines)
-    boolean d_line_top_side = double_line_top_side();
-    boolean d_line_bottom_side = double_line_bottom_side();
-    boolean d_line_left_side = double_line_left_side();
-    boolean d_line_right_side = double_line_right_side();
     for(int i =0; i < 10; i++){
+      boolean d_line_top_side = double_line_top_side();
+      boolean d_line_bottom_side = double_line_bottom_side();
+      boolean d_line_left_side = double_line_left_side();
+      boolean d_line_right_side = double_line_right_side();
       if ( d_line_top_side ) { move_back(100); continue;}
       if ( d_line_bottom_side ) { move_forward(100); continue; }
-      if ( d_line_left_side ) { move_right(100); continue; }
-      if ( d_line_right_side ) { move_left(100); continue; }
+      if ( d_line_left_side ) { move_right(300); continue; }
+      if ( d_line_right_side ) { move_left(300); continue; }
       break;
     }
+    Serial.println("soft shift_to_center");
     // 2. soft shift to center circle (same sides)
     for(int i =0; i < 10; i++){
       float top_side = get_top_side();
@@ -83,70 +85,120 @@ void shift_to_center() { // add error to 10
       float left_side = get_left_side();
       float right_side = get_right_side();
       // same sides
-      if (top_side > 0 && bottom_side < 0) { move_right(100); continue; }
-      if (top_side < 0 && bottom_side > 0) { move_left(100); continue; }
       if (left_side > 0 && right_side < 0) { move_forward(100); continue; }
       if (left_side < 0 && right_side > 0) { move_back(100); continue; }
+      if (top_side > 0 && bottom_side < 0) { move_right(300); continue; }
+      if (top_side < 0 && bottom_side > 0) { move_left(300); continue; }
       // different sides
-      if ( (top_side > 0 && bottom_side > 0) && (abs(top_side) < abs(bottom_side) ) ) { move_left(100); continue; }
-      if ( (top_side > 0 && bottom_side > 0) && (abs(top_side) > abs(bottom_side) ) ) { move_right(100); continue; }
-      if ( (top_side < 0 && bottom_side < 0) && (abs(top_side) < abs(bottom_side) ) ) { move_right(100); continue; }
-      if ( (top_side < 0 && bottom_side < 0) && (abs(top_side) > abs(bottom_side) ) )  {  move_left(100); continue; }
       if ( (left_side > 0 && right_side > 0) && (abs(left_side) < abs(right_side) ) ) {  move_back(100); continue; }
       if ( (left_side > 0 && right_side > 0) && (abs(left_side) > abs(right_side) ) ) {  move_forward(100); continue; }
       if ( (left_side < 0 && right_side < 0) && (abs(left_side) < abs(right_side) ) ) {  move_forward(100); continue; }
       if ( (left_side < 0 && right_side < 0) && (abs(left_side) > abs(right_side) ) ) {  move_back(100); continue; }
+      if ( (top_side > 0 && bottom_side > 0) && (abs(top_side) < abs(bottom_side) ) ) { move_left(300); continue; }
+      if ( (top_side > 0 && bottom_side > 0) && (abs(top_side) > abs(bottom_side) ) ) { move_right(300); continue; }
+      if ( (top_side < 0 && bottom_side < 0) && (abs(top_side) < abs(bottom_side) ) ) { move_right(300); continue; }
+      if ( (top_side < 0 && bottom_side < 0) && (abs(top_side) > abs(bottom_side) ) )  {  move_left(300); continue; }
       break;
     }
 }
 
 void rotate_to_closer_line() {
+  Serial.println("rotate_to_closer_line");
   //1. turn circle
-  for(int i =0; i < 10; i++){
+  for(int i =0; i < 15; i++){
     shift_to_center();
     float top_side = get_top_side();
-    if (top_side < 0) turn_left(100);
-    if (top_side > 0) turn_right(100);
+    float bottom_side = get_bottom_side();
+    float left_side = get_left_side();
+    float right_side = get_right_side();
+    if (top_side == -bottom_side && top_side < 0 ) { turn_left(200); continue; }
+    if (-top_side == bottom_side && top_side > 0 ) { turn_right(200); continue; }
+    if (top_side == 0 && bottom_side < 0) { turn_r_f(300); continue; }
+    if (top_side == 0 && bottom_side > 0) { turn_l_f(300); continue; }
+    if (bottom_side == 0 && top_side < 0) { turn_r_b(300); continue; }
+    if (bottom_side == 0 && top_side > 0) { turn_l_b(300); continue; }
+
+    if (top_side == right_side && bottom_side == left_side ) { turn_left(200); continue; }
+    if (top_side == left_side && bottom_side == right_side ) { turn_right(200); continue; }
+    break;
   }
 }
 
 void rotate_to_left_line() {
-  turn_left(100);
+  Serial.println("rotate_to_left_line");
+  turn_left(300);
   //1. turn circle
   for(int i =0; i < 10; i++){
     shift_to_center();
     float top_side = get_top_side();
-    if (top_side > 0) turn_left(100);
+    if (top_side > 0) turn_left(300);
   }
 }
 
 void rotate_to_right_line() {
-  turn_right(100);
+  Serial.println("rotate_to_right_line");
+  turn_right(300);
   //1. turn circle
   for(int i =0; i < 10; i++){
     shift_to_center();
     float top_side = get_top_side();
-    if (top_side < 0) turn_right(100);
+    if (top_side < 0) turn_right(300);
   }
 }
 
 void rotate_left_90() {
+    Serial.println("rotate_left_90");
     rotate_to_left_line();
     rotate_to_closer_line();
 }
 
 void rotate_right_90() {
+    Serial.println("rotate_right_90");
     rotate_to_right_line();
     rotate_to_closer_line();
 }
 
 
 // get coordinates
-boolean double_line_top_side() { return false; }
-boolean double_line_bottom_side() { return false; }
-boolean double_line_left_side() { return false; }
-boolean double_line_right_side() { return false; }
+boolean double_line_top_side() {
+  boolean d_line = false; float sum = 0; float avg = 0; int lines = 0;
+  int data[8];
+  for (int i = 0; i < 8; i++) { data[i] = octoliner_top.analogRead(i); sum += data[i];}
+  avg = sum/8;
+  for (int i = 0; i < 8; i++) { if( data[i] > avg && (data[i] - avg) > 20) lines++; }
+  if( lines > 2) d_line = true; if( data[0] > avg && data[7] > avg) d_line = true;
+  return d_line;
+}
+boolean double_line_bottom_side() {
+  boolean d_line = false; float sum = 0; float avg = 0; int lines = 0;
+  int data[8];
+  for (int i = 0; i < 8; i++) { data[i] = octoliner_bottom.analogRead(i); sum += data[i];}
+  avg = sum/8;
+  for (int i = 0; i < 8; i++) { if( data[i] > avg && (data[i] - avg) > 20) lines++; }
+  if( lines > 2) d_line = true; if( data[0] > avg && data[7] > avg) d_line = true;
+  return d_line;
+}
+boolean double_line_left_side() {
+  boolean d_line = false; float sum = 0; float avg = 0; int lines = 0;
+  int data[8];
+  for (int i = 0; i < 8; i++) { data[i] = octoliner_left.analogRead(i); sum += data[i];}
+  avg = sum/8;
+  for (int i = 0; i < 8; i++) { if( data[i] > avg && (data[i] - avg) > 20) lines++; }
+  if( lines > 2) d_line = true; if( data[0] > avg && data[7] > avg) d_line = true;
+  return d_line;
+}
+boolean double_line_right_side() {
+  boolean d_line = false; float sum = 0; float avg = 0; int lines = 0;
+  int data[8];
+  for (int i = 0; i < 8; i++) { data[i] = octoliner_right.analogRead(i); sum += data[i];}
+  avg = sum/8;
+  for (int i = 0; i < 8; i++) { if( data[i] > avg && (data[i] - avg) > 20) lines++; }
+  if( lines > 2) d_line = true; if( data[0] > avg && data[7] > avg) d_line = true;
+  return d_line;
+}
+
 float get_top_side(){
+  //Serial.println("get_top_side");
   int dataSensors_top[8];
   for (int i = 0; i < 8; i++) {
     dataSensors_top[i] = octoliner_top.analogRead(i);
@@ -155,6 +207,7 @@ float get_top_side(){
   return top;
 }
 float get_bottom_side(){
+  //Serial.println("get_bottom_side");
   int dataSensors_bottom[8];
   for (int i = 0; i < 8; i++) {
     dataSensors_bottom[i] = octoliner_bottom.analogRead(i);
@@ -163,6 +216,7 @@ float get_bottom_side(){
   return bottom;
 }
 float get_left_side(){
+  //Serial.println("get_left_side");
   int dataSensors_left[8];
   for (int i = 0; i < 8; i++) {
     dataSensors_left[i] = octoliner_left.analogRead(i);
@@ -171,6 +225,7 @@ float get_left_side(){
   return left;
 }
 float get_right_side(){
+  //Serial.println("get_right_side");
   int dataSensors_right[8];
   for (int i = 0; i < 8; i++) {
     dataSensors_right[i] = octoliner_right.analogRead(i);
@@ -182,66 +237,84 @@ float get_right_side(){
 
 /// wheels moves
 void turn_right(int ms){
+  Serial.println("turn_right");
   myservo1.write(360); //right rev
   myservo2.write(360); //left
   delay(ms);
   myservo1.write(90);
   myservo2.write(90);
+  delay(200);
 }
 void turn_left(int ms){
+  Serial.println("turn_left");
   myservo1.write(0); //right rev
   myservo2.write(0); //left
   delay(ms);
   myservo1.write(90);
   myservo2.write(90);
+  delay(200);
 }
 void move_left(int ms){
+  Serial.println("move_left");
   turn_r_f(ms);
   turn_l_f(ms);
   turn_r_b(ms);
   turn_l_b(ms);
 }
 void move_right(int ms){
+  Serial.println("move_right");
   turn_l_f(ms);
   turn_r_f(ms);
   turn_l_b(ms);
   turn_r_b(ms);
 }
 void move_forward(int ms){
+  Serial.println("move_forward");
   myservo1.write(360);
   myservo2.write(0);
   delay(ms);
   myservo1.write(90);
   myservo2.write(90);
+  delay(200);
 }
 void move_back(int ms){
+  Serial.println("move_back");
   myservo1.write(0);
   myservo2.write(360);
   delay(ms);
   myservo1.write(90);
   myservo2.write(90);
+  delay(200);
 }
 void turn_r_f(int ms){
+  Serial.println("turn_r_f");
   myservo2.write(0);
   delay(ms);
   myservo1.write(90);
   myservo2.write(90);
+  delay(200);
 }
 void turn_l_f(int ms){
+  Serial.println("turn_l_f");
   myservo1.write(360);
   delay(ms);
   myservo1.write(90);
   myservo2.write(90);
+  delay(200);
 }
 void turn_r_b(int ms){
+  Serial.println("turn_r_b");
   myservo2.write(360);
   delay(ms);
   myservo1.write(90);
   myservo2.write(90);
+  delay(200);
 }
 void turn_l_b(int ms){
+  Serial.println("turn_l_b");
   myservo1.write(0);
   delay(ms);
   myservo1.write(90);
   myservo2.write(90);
+  delay(200);
 }
